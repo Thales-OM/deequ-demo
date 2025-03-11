78213190408e915e92971a07229b91b6965f50ad
Thales-OM <pugin2011ilya@gmail.com>
2025-03-11 12:58:36 +0300

    add time logging

--- spark-cleansed/build.sbt
+++ spark-cleansed/build.sbt
@@ -11,7 +11,9 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "3.3.0",
   "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.3.0", // Required for Kafka integration
   "org.apache.kafka" %% "kafka" % "2.8.1", // Kafka client library
-  "com.amazon.deequ" % "deequ" % "2.0.8-spark-3.3" // Deequ for Spark 3.3
+  "com.amazon.deequ" % "deequ" % "2.0.8-spark-3.3", // Deequ for Spark 3.3
+  "org.slf4j" % "slf4j-api" % "1.7.36", // SLF4J API (stable version for Scala 2.12)
+  "ch.qos.logback" % "logback-classic" % "1.2.12" // Logback implementation (stable version for Scala 2.12)
 )
 
 resolvers ++= Seq(
--- spark-cleansed/src/main/scala/com/spark_app/SparkStructuredStreaming.scala
+++ spark-cleansed/src/main/scala/com/spark_app/SparkStructuredStreaming.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.streaming.Trigger
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types.{IntegerType, StringType, TimestampType}
 import com.amazon.deequ.VerificationSuite
 import com.amazon.deequ.VerificationResult
 import com.amazon.deequ.checks.Check
@@ -12,10 +13,13 @@ import com.amazon.deequ.checks.CheckResult
 import com.amazon.deequ.checks.CheckStatus
 import com.amazon.deequ.checks.CheckLevel
 import com.amazon.deequ.constraints.ConstraintStatus
+import com.amazon.deequ.schema.{RowLevelSchema,RowLevelSchemaValidator}
+import org.slf4j.LoggerFactory
 
 object SparkStructuredStreaming {
 
   def main(args: Array[String]): Unit = {
+    private val logger = LoggerFactory.getLogger(getClass)
 
     val spark = SparkSession
       .builder()
@@ -59,23 +63,46 @@ object SparkStructuredStreaming {
     val target_table = sys.env.getOrElse("SPARK_TARGET_TABLE", "target_table")
 
     // Define a function to perform Deequ data quality checks
-    def performDeequChecks(dataFrame: DataFrame): Unit = {
-      println("Data quality checks passed!")
+    def performDeequChecks(dataFrame: DataFrame, batchId: Long): Unit = {
+      val startTime = System.currentTimeMillis()
+      val schema = RowLevelSchema()
+        .withIntColumn("id", isNullable = false)
+        .withStringColumn("type", isNullable = false, maxLength = Some(20))
+        .withTimestampColumn("timestamp", mask = "yyyy-MM-dd HH:mm:ss", isNullable = false)
+
+      val result = RowLevelSchemaValidator.validate(data, schema)
+
+      val durationInSeconds = (System.currentTimeMillis() - startTime) / 1000.0 
+      totalDeequChecksTime += durationInSeconds
+      logger.info(s"[Batch #$batchId] Deequ validation completed in $durationInSeconds sec. (Total Deequ time = $totalDeequChecksTime sec)")
     }
 
+    def process_batch(batchDF: DataFrame, batchId: Long): Unit = {
+      performDeequChecks(batchDF, batchId)
+      
+      // Write the batch DataFrame to the target table
+      batchDF.write
+        .format("parquet") // You can change this to "delta" or another format if needed
+        .mode("append")
+        .option("path", outputPath)
+        .saveAsTable(target_table)
+      
+      val endTime = System.currentTimeMillis()
+      val durationInSeconds = (endTime - lastTotalCheckpointTime) / 1000.0 
+      val totaldurationInSeconds = (endTime - totalStartTime) / 1000.0 
+      lastTotalCheckpointTime = endTime
+      logger.info(s"[Batch #$batchId] Processed and written in $durationInSeconds sec. (Total work time = $totaldurationInSeconds sec)")
+    }
+
+    val totalStartTime = System.currentTimeMillis()
+    val lastTotalCheckpointTime = totalStartTime
+    val totalDeequChecksTime: BigDecimal = BigDecimal(0.0)
     // Write to a Spark table (TARGET_TABLE)
     val query = parsedValues.writeStream
       .outputMode("append")
       .foreachBatch { (batchDF: DataFrame, batchId: Long) =>
-        // Perform Deequ data quality checks on each batch
-        performDeequChecks(batchDF)
-
-        // Write the batch DataFrame to the target table
-        batchDF.write
-          .format("parquet") // You can change this to "delta" or another format if needed
-          .mode("append")
-          .option("path", outputPath)
-          .saveAsTable(target_table)
+        // Perform Deequ data quality checks on each batch an write data to table
+        process_batch(batchDF, batchId)
       }
       .option("checkpointLocation", checkpointPath) // Specify checkpoint location from environment variable
       .trigger(Trigger.ProcessingTime("1 second")) // Adjust the trigger interval for faster processing
